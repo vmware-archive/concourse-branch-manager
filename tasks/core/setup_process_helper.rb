fb4781e3b6fc0520794283f56bc8f52cc75a9162
Baker Bokorney <bbokorney@gmail.com>
2017-03-06 16:46:42 -0500

    Use specific version of process_helper gem

--- tasks/core/setup_process_helper.rb
+++ tasks/core/setup_process_helper.rb
@@ -1,4 +1,4 @@
-system('gem install process_helper --no-ri --no-rdoc') ||
+system('gem install process_helper --no-ri --no-rdoc --version 0.0.3') ||
   fail('failed to install process_helper gem')
 require 'rubygems'
 require 'process_helper'
